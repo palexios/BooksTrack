502ff169a7293652509901eb707247c7f2ff5eb7
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-11-05 22:55:20 +0300

    prepared the project

---
+++ BooksTrack/Scenes/MainTabBar/Composition/MainTabBarCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabBarCoordinator.swift
+//  BooksTrack
+//
+//  Created by Александр Павлицкий on 05.11.2024.
+//
+
+import Foundation
---
+++ BooksTrack/Scenes/MainTabBar/Presentation/MainTabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabBarController.swift
+//  BooksTrack
+//
+//  Created by Александр Павлицкий on 05.11.2024.
+//
+
+import Foundation
