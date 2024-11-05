1c8b28b30264925529b5745d1024385a0cd376af
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-11-05 22:56:35 +0300

    made MainTabBarCoordinator

--- BooksTrack/Scenes/MainTabBar/Composition/MainTabBarCoordinator.swift
+++ BooksTrack/Scenes/MainTabBar/Composition/MainTabBarCoordinator.swift
@@ -1,8 +1,21 @@
-//
-//  MainTabBarCoordinator.swift
-//  BooksTrack
-//
-//  Created by Александр Павлицкий on 05.11.2024.
-//
+import UIKit
 
-import Foundation
+// MARK: - MainTabBarCoordinator
+final class MainTabBarCoordinator: Coordinator {
+    // MARK: - Properties
+    var navigationController: UINavigationController
+    var childrenCoordinators: [Coordinator] = []
+    
+    private let factory: MainTabBarFactory
+    
+    // MARK: - Init
+    init(navigationController: UINavigationController, factory: MainTabBarFactory) {
+        self.navigationController = navigationController
+        self.factory = factory
+    }
+    
+    // MARK: - Methods
+    func start() {
+        
+    }
+}
